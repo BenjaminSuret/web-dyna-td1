a4e11b17677172b162556ffe4dbb2d48314290ce
Benjamin.Suret <21905911@etu.unicaen.fr>
2020-03-18 14:20:07 +0100

    ex1

--- td1/ex1.php
+++ td1/ex1.php
@@ -4,12 +4,12 @@
 	<meta charset="UTF-8">
 	<title>Hello</title>
 </head>
-<a href="?message=lol">1er message</a>
+<a href="?message=1">1er message</a>
+<a href="?message=2">2ème message</a>
+<a href="?message=3">3ème message</a>
 
 <body>
 	<?php
-	echo("Hello world !");
-	echo "Vous êtes sur la page n° ".$_GET["page"];
 	echo "Vous êtes sur la page n° ".$_GET["message"];
 	?>
 </body>
