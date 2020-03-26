23a39a30550f95cca2b9ef26c13d7a724f5096f3
BenjaminSuret <62335782+BenjaminSuret@users.noreply.github.com>
2020-03-26 16:50:58 +0100

    Add files via upload

--- td1/ex4.php
+++ td1/ex4.php
@@ -2,35 +2,33 @@
 include "header.php";
 ?>
 
+<body>
+
 <a href="?lignes=2&colonnes=2">Le tableau 2x2</a>
 <a href="?lignes=5&colonnes=5">Le tableau 5x5</a>
 <a href="?lignes=10&colonnes=8">Le tableau 10x8</a>
 
-<body>
-	
-		<table border='1'>
 
-	<tbody>
-		<?php 
-			for($i=0;$i<$lignes;$i++)
-			{
-		?>
-				<tr>
+<?php
+    echo "<table border='1'>
+  			<tbody>";
+    			for($a=1;$a<=$_GET["lignes"];$a++)
+    			    { 
+						if (($a % 2) == 0)
+    					{
+            				$gras = "bold";
+            			} 
+       					echo "<tr>"; 
+        				for($b=1;$b<=$_GET["colonnes"];$b++)
+        					{ 
+            					echo "<td> <p style='font-weight: bold;'>$a-$b</p> </td>";
+        					} 
+        				echo "</tr>"; 
+    				} 
+			echo "</tbody>
+    </table>";
+?>    
 
-        <?php 
-        	for($j=0;$j<$nbColonnes;$j++){
-        ?>
-
-            
-        <?php }
-        ?>
-        </tr>
-
-      <?php 
-  }
-  ?>
-	?>
-
-Contenu de la page
 <?php
 include "footer.php";
+?>
